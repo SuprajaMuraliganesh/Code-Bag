ab71bcb416a66afb48fc3b5fb0821aac7eecb315
SuprajaMuraliganesh <30307855+SuprajaMuraliganesh@users.noreply.github.com>
2017-07-20 10:15:11 +0530

    Create numbers.c

---
+++ numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("Enter the value:);
+scanf("%d",&n);
+if(n==0)
+printf("The number is Zero");
+else if(n>0)
+printf("The number is Positive");
+else 
+printf("The number is Negative");
+}
