6fb4424580685848ebce833256965d210eceb51f
SuprajaMuraliganesh <30307855+SuprajaMuraliganesh@users.noreply.github.com>
2017-07-20 10:35:09 +0530

    Update evenodd.c

--- evenodd.c
+++ evenodd.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 void main()
 {
-int n;
-printf("Enter the number:");
-scanf("%d",&n);
-if(n%2==0)
-printf("The number is even");
-else
-printf("The number is odd");
+ int n;
+ printf("Enter the number:");
+ scanf("%d",&n);
+  if(n>0)
+  {
+   if(n%2==0)
+   printf("The number is even");
+   else
+    printf("The number is odd");
+}
+  else
+    printf("Enter a positive number");
 }
