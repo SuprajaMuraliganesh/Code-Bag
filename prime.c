24575d74158f37f7e395ad03beb7f7d91880e062
SuprajaMuraliganesh <30307855+SuprajaMuraliganesh@users.noreply.github.com>
2017-08-11 16:56:31 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 void main()
 {
-    int n, i, flag=0;
-    printf("Enter a positive integer: ");
-    scanf("%d",&n);
-    for(i=2; i<=n/2; ++i)
+    int low, high, i, flag;
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d", &low, &high);
+    printf("Prime numbers between %d and %d are: ", low, high);
+    while (low < high)
     {
-      if(n%i==0)
+        flag = 0;
+        for(i = 2; i <= low/2; ++i)
         {
-            flag=1;
-            break;
+            if(low % i == 0)
+            {
+                flag = 1;
+                break;
+            }
         }
+
+        if (flag == 0)
+            printf("%d ", low);
+
+        ++low;
     }
-    if (flag==0)
-        printf("%d is a prime number.",n);
-    else
-        printf("%d is not a prime number.",n);
-    
+
 }
