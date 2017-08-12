ed4cc82cdcf5aafc74199e92480b0b897516e57c
SuprajaMuraliganesh <30307855+SuprajaMuraliganesh@users.noreply.github.com>
2017-08-12 16:26:34 +0530

    Create hcf.c

---
+++ hcf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int hcf(int, int);
+void main()
+{
+    int a, b, res;
+    printf("Enter the two numbers to find their HCF: ");
+    scanf("%d%d", &a, &b);
+    result = hcf(a, b);
+    printf("The HCF of %d and %d is %d.\n", a, b, res);
+}
+int hcf(int a, int b)
+{
+    while (a != b)
+    {
+        if (a > b)
+        {
+            return hcf(a - b, b);
+        }
+        else
+        {
+            return hcf(a, b - a);
+        }
+    }
+}
