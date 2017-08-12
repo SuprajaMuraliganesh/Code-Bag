7b5e142253be0303b82f2e627f8264b450977b9c
SuprajaMuraliganesh <30307855+SuprajaMuraliganesh@users.noreply.github.com>
2017-08-12 16:56:36 +0530

    Create revnum.c

---
+++ revnum.c
@@ -0,0 +1,25 @@
+                      #include<stdio.h>
+void main()
+{
+int i=0,m,n,a,rev[10];
+printf("Enter the number:");
+scanf("%d",&n);
+while(n!=0)
+{
+a=n%10;
+rev[i]=a;
+i++;
+n=n/10;
+}
+m=i;
+printf("The reverse of a number is ");
+for(i=0;i<m;i++)
+{
+printf("%d",rev[i]);
+}
+}
+
+
+
+
+
